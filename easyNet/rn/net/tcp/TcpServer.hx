7ba075dfc4d958db4cf8d8f97958ef0d98d4e126
r3d9u11 <r3d9u11@gmail.com>
2018-01-30 20:44:21 +0300

    tcp-server

--- easyNet/rn/net/tcp/TcpServer.hx
+++ easyNet/rn/net/tcp/TcpServer.hx
@@ -1,8 +1,10 @@
 package rn.net.tcp;
 
 #if cpp
+	import cpp.vm.Mutex;
 	import cpp.vm.Thread;
 #elseif neko
+	import neko.vm.Mutex;
 	import neko.vm.Thread;
 #end
 
@@ -15,16 +17,24 @@ class TcpServer {
 	
 	var tcpListener:TcpSocket;
 	var listenderThread:Thread;
+	var clientCheckerThread:Thread;
 	
+	//------------------------------------------------------------------------------------------
+	
+	public var started (get, null) : Bool;
+	function get_started () return if (tcpListener != null) tcpListener.connected else false;
+
 	//------------------------------------------------------------------------------------------
 	
 	var clients:Map<String, ITcpClient>;
 	var clientThreads:Map<String, Thread>;
+	var clientsLocker:Mutex;
 	
 	//------------------------------------------------------------------------------------------
 	
 	public dynamic function onClientBeforeConnect (client:ITcpClient) { }
 	public dynamic function onClientAfterConnect (clientGuid:String) { }
+	public dynamic function onClientDisconnect (clientGuid:String) { }
 
 	public dynamic function clientCheckingProc (client:ITcpClient) : Bool return true;
 
@@ -38,6 +48,8 @@ class TcpServer {
 
 		this.tcpListener = tcpListener != null ? tcpListener : new TcpSocket(null, inputBufferSize);
 		listenderThread = null;
+		clientCheckerThread = null;
+		clientsLocker = new Mutex();
 
 		clients = new Map<String, ITcpClient>();
 		clientThreads = new Map<String, Thread>();
@@ -45,6 +57,22 @@ class TcpServer {
 	
 	//------------------------------------------------------------------------------------------
 	
+	function clientsAutocheckingProc () {
+		while (started) {
+			//clientsLocker.acquire();
+			
+			for (clientGuid in clients.keys())
+				if (!clients[clientGuid].connected) {
+					disconnect(clientGuid);
+					break;
+				}
+			
+			//clientsLocker.release();
+			
+			Sys.sleep(.1);
+		}
+	}
+
 	function clientProc () {
 		var clientGuid:String = Thread.readMessage(true);
 		var tcpClient = clients.exists(clientGuid) ? clients[clientGuid] : null;
@@ -77,12 +105,16 @@ class TcpServer {
 				if (tcpClient != null)
 					if (tcpClient.connected) {
 						if (clientCheckingProc(tcpClient)) {
+							//clientsLocker.acquire();
+							
 							var clientGuid = DateTools.format(Date.now(), "%Y%m%d%H%M%S") + (Math.random() * Lambda.count(clients)) + Lambda.count(clients);
 							
 							clients.set(clientGuid, tcpClient);
 							
 							var clientThread = Thread.create(clientProc);
 							clientThread.sendMessage(clientGuid);
+
+							//clientsLocker.release();
 
 							clientThreads.set(clientGuid, clientThread);
 
@@ -95,13 +127,16 @@ class TcpServer {
 
 	//------------------------------------------------------------------------------------------
 
-	public function start (host:sys.net.Host, port:Int) {
-		if (tcpListener.bind(host, port)) {
-			listenderThread = Thread.create(listenderProc);
-			return true;
-		}
+	public function start (host:sys.net.Host, port:Int, clientsAutochecking:Bool = true) {
+		var result = false;
 
-		return false;
+		if (result = tcpListener.bind(host, port)) 
+			listenderThread = Thread.create(listenderProc);
+
+		if (result && clientsAutochecking)
+			clientCheckerThread = Thread.create(clientsAutocheckingProc);
+
+		return result;
 	}
 	
 	public function stop () {
@@ -110,38 +145,62 @@ class TcpServer {
 
 		tcpListener = null;
 		listenderThread = null;
+		clientCheckerThread = null;
 		
 		disconnectAll();
 	}
 
 	//------------------------------------------------------------------------------------------
 
-	public function disconnectAll () for (clientGuid in clients.keys()) disconnect(clientGuid);
+	public function disconnectAll () {
+		//clientsLocker.acquire();
+		
+		for (clientGuid in clients.keys())
+			disconnect(clientGuid);
+		
+		//clientsLocker.release();
+	}
 	
 	public function disconnect (clientGuid:String) {
-		if (clients.exists(clientGuid)) {
-			clients[clientGuid].disconnect();
-			clients.remove(clientGuid);
-		}
-		
+		//clientsLocker.acquire();
+
 		if (clientThreads.exists(clientGuid))
 			clientThreads.remove(clientGuid);
+
+		if (clients.exists(clientGuid))
+			clients.remove(clientGuid);
+
+		//clientsLocker.release();
+
+		onClientDisconnect(clientGuid);
 	}
 
 	//------------------------------------------------------------------------------------------
 
 	public function sendData (clientGuid:String, data:Bytes) {
-		if (clients.exists(clientGuid))
-			return clients[clientGuid].sendData(data);
+		var result = false;
 
-		return false;
+		//clientsLocker.acquire();
+
+		if (clients.exists(clientGuid))
+			result = clients[clientGuid].sendData(data);
+
+		//clientsLocker.release();
+
+		return result;
 	}
 
 	public function sendText (clientGuid:String, text:String) {
+		var result = false;
+
+		//clientsLocker.acquire();
+
 		if (clients.exists(clientGuid))
 			return clients[clientGuid].sendText(text);
 
-		return false;
+		//clientsLocker.release();
+
+		return result;
 	}
 
 	public function sendTextLine (clientGuid:String, text:String)
--- samples/tcp/tcpclient/server/Source/Main.hx
+++ samples/tcp/tcpclient/server/Source/Main.hx
@@ -10,12 +10,13 @@ class Main {
 		
 		server.onClientBeforeConnect = function (client:ITcpClient) trace ('client connected');
 		server.onClientAfterConnect = function (clientUuid:String) server.sendTextLine(clientUuid, "hello from server!");
-		
+		server.onClientDisconnect = function (clientUuid:String) trace('client $clientUuid disconnected');
 		server.onClientText = function (clientUuid:String, text:String) trace ('client: $text');
 		
 		if (server.start(new Host("localhost"), 5000)) {
 			trace("server started at 5000");
 			trace("press any key to exit...");
+			
 			Sys.getChar(false);
 		}
 		else
